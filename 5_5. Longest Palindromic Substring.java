2b4f34912496b61c6e60077b21baf961e1ec5fc2
yumuzi <yumuzi234@gmail.com>
2017-03-30 20:18:25 -0700

    3/30/2017

---
+++ 5_5. Longest Palindromic Substring.java
@@ -0,0 +1,104 @@
+// Given a string s, find the longest palindromic substring in s. You may assume that the maximum length of s is 1000.
+
+// Example:
+
+// Input: "babad"
+
+// Output: "bab"
+
+// Note: "aba" is also a valid answer.
+
+// https://en.wikipedia.org/wiki/Longest_palindromic_substring
+
+// O(n) solution from wiki, developed in 1990s...
+
+ public static String findLongestPalindrome(String s) {
+        if (s==null || s.length()==0)
+            return "";
+        
+        char[] s2 = addBoundaries(s.toCharArray());
+        int[] p = new int[s2.length]; 
+        int c = 0, r = 0; // Here the first element in s2 has been processed.
+        int m = 0, n = 0; // The walking indices to compare if two elements are the same
+        for (int i = 1; i<s2.length; i++) {
+            if (i>r) {
+                p[i] = 0; m = i-1; n = i+1;
+            } else {
+                int i2 = c*2-i;
+                if (p[i2]<(r-i-1)) {
+                    p[i] = p[i2];
+                    m = -1; // This signals bypassing the while loop below. 
+                } else {
+                    p[i] = r-i;
+                    n = r+1; m = i*2-n;
+                }
+            }
+            while (m>=0 && n<s2.length && s2[m]==s2[n]) {
+                p[i]++; m--; n++;
+            }
+            if ((i+p[i])>r) {
+                c = i; r = i+p[i];
+            }
+        }
+        int len = 0; c = 0;
+        for (int i = 1; i<s2.length; i++) {
+            if (len<p[i]) {
+                len = p[i]; c = i;
+            }
+        }
+        char[] ss = Arrays.copyOfRange(s2, c-len, c+len+1);
+        return String.valueOf(removeBoundaries(ss));
+    }
+ 
+    private static char[] addBoundaries(char[] cs) {
+        if (cs==null || cs.length==0)
+            return "||".toCharArray();
+
+        char[] cs2 = new char[cs.length*2+1];
+        for (int i = 0; i<(cs2.length-1); i = i+2) {
+            cs2[i] = '|';
+            cs2[i+1] = cs[i/2];
+        }
+        cs2[cs2.length-1] = '|';
+        return cs2;
+    }
+
+    private static char[] removeBoundaries(char[] cs) {
+        if (cs==null || cs.length<3)
+            return "".toCharArray();
+
+        char[] cs2 = new char[(cs.length-1)/2];
+        for (int i = 0; i<cs2.length; i++) {
+            cs2[i] = cs[i*2+1];
+        }
+        return cs2;
+    }    
+}
+
+// O(n2) solution
+public String longestPalindrome(String s) {
+        if (s.length()<2) {
+            return s;
+        }
+        int n=s.length();
+        int len=0;
+        String res="";
+        boolean dp[][]=new boolean[n][n];
+        for (int i=n-1; i>=0; i--) {
+            for (int j=i; j<n; j++) {
+                if (j==i) {
+                    dp[i][j]=true;
+                } else if (j==i+1) {
+                    dp[i][j]=s.charAt(i)==s.charAt(j);
+                } else {
+                    dp[i][j]=(s.charAt(i)==s.charAt(j) && dp[i+1][j-1]);
+                }
+                if  (dp[i][j] && len<j-i+1) {
+                    res=s.substring(i, j+1);
+                    len=j-i+1;
+                }
+                
+            }
+        }
+        return res;
+    }
