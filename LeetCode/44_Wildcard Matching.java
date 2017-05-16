ec8436885093852d0dad84404ccd108c22babbd4
YsGH <liying175@gmail.com>
2017-05-15 23:15:24 -0400

    Update 44_Wildcard Matching.java

--- LeetCode/44_Wildcard Matching.java
+++ LeetCode/44_Wildcard Matching.java
@@ -1,21 +1,59 @@
-// plement wildcard pattern matching with support for '?' and '*'.
+/*
+题目： plement wildcard pattern matching with support for '?' and '*'.
 
-// '?' Matches any single character.
-// '*' Matches any sequence of characters (including the empty sequence).
+'?' Matches any single character.
+'*' Matches any sequence of characters (including the empty sequence).
 
-// The matching should cover the entire input string (not partial).
+The matching should cover the entire input string (not partial).
 
-// The function prototype should be:
-// bool isMatch(const char *s, const char *p)
+The function prototype should be:
+bool isMatch(const char *s, const char *p)
 
-// Some examples:
-// isMatch("aa","a") → false
-// isMatch("aa","aa") → true
-// isMatch("aaa","aa") → false
-// isMatch("aa", "*") → true
-// isMatch("aa", "a*") → true
-// isMatch("ab", "?*") → true
-// isMatch("aab", "c*a*b") → false
+Some examples:
+isMatch("aa","a") → false
+isMatch("aa","aa") → true
+isMatch("aaa","aa") → false
+isMatch("aa", "*") → true
+isMatch("aa", "a*") → true
+isMatch("ab", "?*") → true
+isMatch("aab", "c*a*b") → false
+
+＃recursion #dp #greedy #regex #wildcard #two pointers
+*/
+Solution 1: recursion
+Solution 2: dp
+
+Solution 3: greedy
+纪录上一个＊号位置，然后先match 0个char，如果不能match下去，再match多一个
+public boolean isMatch(String s, String p) {
+	int i = 0;
+	int j = 0;
+	int starIndex = -1;
+	int iIndex = -1;
+ 
+	while (i < s.length()) {
+		if (j < p.length() && (p.charAt(j) == '?' || p.charAt(j) == s.charAt(i))) {
+			++i;
+			++j;
+		} else if (j < p.length() && p.charAt(j) == '*') {
+			starIndex = j;		
+			iIndex = i;
+			j++;
+		} else if (starIndex != -1) {
+			j = starIndex + 1;
+			i = iIndex+1;
+			iIndex++;
+		} else {
+			return false;
+		}
+	}
+ 
+	while (j < p.length() && p.charAt(j) == '*') {
+		++j;
+	}
+ 
+	return j == p.length();
+}
 
 public class Solution {
     public boolean isMatch(String s, String p) {
